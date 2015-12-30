f04c8f0e201da26b948641186737e84502347b04
Mottaqui Karim <taqqui.karim@gmail.com>
2015-12-29 23:47:52 -0500

    added comments + logging

--- app/generator.js
+++ app/generator.js
@@ -1,3 +1,4 @@
+var LogComment = require('./logcomment');
 var FileOps = require('./fileops');
 var marked = require('marked');
 var path = require('path');
@@ -12,12 +13,9 @@ var __ = {};
 
 __.run = function run( basePath, outputPath ) {
     var configPath = path.join( basePath, CONSTS.config );
-    FileOps.openFile( configPath ).then(function(data) {
+    return FileOps.openFile( configPath ).then(function(data) {
         data = JSON.parse( data );
         return __.readFiles( basePath, data, outputPath );
-    })
-    .then(function(files) {
-
     });
 }
 
@@ -26,53 +24,108 @@ __.readFiles = function( basePath, data, outputPath ) {
 
     var files = data.data;
     var staticPath = path.join( basePath, data.staticBase );
-
     var transformedData = {};
-    Object.keys( files ).reduce(function( promise, file ) {
-        return promise.then(function() {
-            var fileData = files[ file ];
-            console.log( file, fileData );
-            return Object.keys( fileData ).reduce(function( fileDataPromise, fileDataItem ) {
-                return fileDataPromise.then(function() {
-                    return FileOps.openFile( path.join( basePath, fileData[ fileDataItem ] ) )
-                    .then(function(fileDataItemData) {
-                        if ( typeof transformedData[ path.join( outputPath, file ) ] === "undefined" ) {
-                            transformedData[ path.join( outputPath, file ) ] = {};
-                        }
 
-                        transformedData[ path.join( outputPath, file ) ][ fileDataItem ] = marked( fileDataItemData );
-                    });
+    // grab all files
+    Object.keys( files )
+    // and begin looping
+    .reduce(function( promise, file ) {
+        // for each file item
+        return promise.then(function() {
+            // grab the data bits
+            var fileData = files[ file ];
+
+            // grab all data bit keys 
+            return Object.keys( fileData )
+            // loop through each one
+            .reduce(function( fileDataPromise, fileDataItem ) {
+                // for each data bit item
+                return fileDataPromise
+                // open the file to read
+                .then(function() {
+
+                    // START: log to terminal
+                    LogComment( 'Opening ' + fileData[ fileDataItem ], 3 );
+                    // END: log to terminal
+
+                    return FileOps.openFile(path.join(
+                        basePath,
+                        fileData[ fileDataItem ]
+                    ));
+                })
+                // then, populate a new object with the data from each data bit item file
+                .then(function(fileDataItemData) {
+                    var outputFilePath = path.join( outputPath, file );
+                    var isUndefined = typeof transformedData[ outputFilePath ] === "undefined";
+                    var markedData = marked( fileDataItemData );
+
+                    if ( isUndefined ) {
+                        transformedData[ outputFilePath ] = {};
+                    }
+
+                    // START: log to terminal
+                    LogComment( 'Completed reading' + file + '[' + fileDataItem + ']', 1 );
+                    // END: log to terminal
+
+                    transformedData[ outputFilePath ][ fileDataItem ] = markedData;
                 });
             }, Q());
         });
     }, Q())
+    // once the new object has been assembled, copy over the static files to output target
     .then(function() {
-        console.log('$$$');
-        console.log( staticPath, outputPath );
+
+        // START: log to terminal
+        LogComment( 'Copying ' + staticPath + ' to ' + outputPath, 0 );
+        // END: log to terminal
+
         return FileOps.copyFile( staticPath, outputPath );
     })
+    // now last step: populating the output target files with markdown data...
     .then(function( err ) {
-        console.log( '-------' );
-        console.log( transformedData );
-        console.log( '-------' );
 
-        Object.keys( transformedData ).reduce(function( tProm, dataItem ) {
+        // START: log to terminal
+        LogComment( 'Copying completed', 1 );
+        // END: log to terminal
+
+        // grab each file in transformed, new object
+        return Object.keys( transformedData )
+        // and loop...
+        .reduce(function( tProm, dataItem ) {
+            // for each file...
             return tProm.then(function() {
+
+                // START: log to terminal
+                LogComment( 'Opening ' + dataItem, 3 );
+                // END: log to terminal
+
+                // read the contents of that file (which should point to output target)
                 return FileOps.openFile( dataItem );
             })
+            // once read, apply underscore template
             .then(function( data ) {
-                console.log( dataItem, transformedData[ dataItem ] );
+
+                // START: log to terminal
+                LogComment( 'Opened ' + dataItem, 1 );
+                LogComment( 'Transforming ' + dataItem + ' for write operation', 3 );
+                // END: log to terminal
+
                 var compiled = _.template( data );
                 var transformed = compiled( transformedData[ dataItem ] );
-                console.log( transformed );
-                return FileOps.writeFile( dataItem, transformed );
+                return FileOps.writeFile( dataItem, transformed )
+                .then(function() {
+                    // START: log to terminal
+                    LogComment( 'Wrote ' + dataItem, 1 );
+                    // END: log to terminal
+                });
             });
-        }, Q())
-        .then(function() {
-            console.log('done!!!');
-        });
+        }, Q());
+    })
+    // done!
+    .then(function() {
+        // resolve and laugh all the way to the bank...
+        d.resolve();
     });
-    console.log( files, staticPath, outputPath );
 
     return d.promise;
 }
--- app/index.js
+++ app/index.js
@@ -1,13 +1,14 @@
+var LogComment = require('./logcomment');
 var Generator = require('./generator');
 var program = require('commander');
 var FileOps = require('./fileops');
 var path = require('path');
+var Q = require('q');
 var CONSTS = {
     'base': path.join( __dirname, '..' ),
     'config': 'config.json',
     'up': '..'
 };
-var Q = require('q');
 
 program
   .version('0.0.1')
@@ -16,20 +17,42 @@ program
   .parse(process.argv);
 
 var config = path.join( CONSTS.base, CONSTS.config );
+
+// START: log to terminal
+LogComment( 'Starting process at ', 1, "dddd, MMMM Do YYYY, h:mm:ss a" );
+LogComment( 'Opening config file...', 0 );
+// END: log to terminal
+
 FileOps.openFile( config  )
 .then(function(data) {
     data = JSON.parse( data );
     var themes = path.join( CONSTS.base, data.themeLookup );
 
+    // START: log to terminal
+    LogComment( 'Complete', 1 );
+    LogComment( 'Reading theme directories...', 0 );
+    // END: log to terminal
+
     return FileOps.getDirectories( themes );
 })
 .then(function( data ) {
+
+    // START: log to terminal
+    LogComment( 'Complete', 1 );
+    LogComment( 'Matching chosen theme...'.magenta );
+    // END: log to terminal
+
     var themes = data.dirs;
     var themePath = data.path;
 
     var filteredThemes = themes.filter(function(theme) {
         return theme === program.theme;
     });
+
+    // START: log to terminal
+    LogComment( 'Complete', 1 );
+    LogComment( 'Starting generator...'.magenta );
+    // END: log to terminal
 
     return Generator.run(path.join(
         themePath,
@@ -38,6 +61,12 @@ FileOps.openFile( config  )
         CONSTS.base,
         program.output
     ));
+})
+.then(function() {
+    // START: log to terminal
+    LogComment( 'Completed transforming all files!', 1 );
+    LogComment('Process Completed'.green);
+    // END: log to terminal
 });
 
 
